db3010036eaef6ceda2ba40c68cbbdab51245afe
Andy Klimczak <andyklimczak@fastmail.com>
2026-04-01 22:14:05 -0400

    Initial Commit

---
+++ wristonic Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wristonic Watch App
+//
+//  Created by Andy Klimczak on 4/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wristonic Watch App/wristonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  wristonicApp.swift
+//  wristonic Watch App
+//
+//  Created by Andy Klimczak on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct wristonic_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
